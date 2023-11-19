5ae385f7967efdeee5ebc63eff15abf10c4edb35
Ji Sang Seo <inaki96@naver.com>
2023-11-19 23:44:32 +0900

    :update styles now gets applied as a default

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,2 +1,3 @@
+import "./styles.css";
 export * from "./FullpageContainer";
 export * from "./FullpageElement";
--- packages/react/vite.config.ts
+++ packages/react/vite.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import dts from "vite-plugin-dts";
 import path from "path";
+import { libInjectCss } from "vite-plugin-lib-inject-css";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), dts()],
+  plugins: [react(), libInjectCss(), dts()],
   build: {
     minify: false,
     lib: {
@@ -15,7 +16,14 @@ export default defineConfig({
       formats: ["es", "umd"],
     },
     rollupOptions: {
-      external: ["react"],
+      external: ["react", "react/jsx-runtime"],
+    },
+  },
+  css: {
+    modules: {
+      scopeBehaviour: "global",
+      localsConvention: "camelCase",
+      generateScopedName: "[hash:base64:4]",
     },
   },
 });
